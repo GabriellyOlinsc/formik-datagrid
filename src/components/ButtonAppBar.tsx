3e2adfe1d54e5864c672ae11030047d3ca25a7fb
ga060558 <ga060558@intelbras.com.br>
2024-07-26 10:51:54 -0300

    feat: add material ui home icon

--- src/components/ButtonAppBar.tsx
+++ src/components/ButtonAppBar.tsx
@@ -1,15 +1,28 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+
+interface ButtonAppBarProps {
+    onClick : () => void;
+    title?: string;
+    icon?: React.ElementType;
+}
 
 export default function ButtonAppBar({...props}) {
+    const { onClick, title, icon: Icon } = props;
   
     return (
         <Box sx={{ flexGrow: 1, mb: 10 }}>
             <AppBar sx={{ backgroundColor: '#2d3748' }} position="fixed">
                 <Toolbar >
                     <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>User System</Typography>
-                    <Button onClick={props.onClick} sx={{ fontSize: 20 }} color="inherit">
-                    {props.title}</Button>
+                    {Icon ? (
+                        <IconButton onClick={onClick} color="inherit">
+                            <Icon style={{fontSize: 45}} />
+                        </IconButton>
+                    ) : (
+                        <Button onClick={onClick} sx={{ fontSize: 20 }} color="inherit">
+                            {title}
+                        </Button>
+                    )}
                 </Toolbar>
             </AppBar>
         </Box>
