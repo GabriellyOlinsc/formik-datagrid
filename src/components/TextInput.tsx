577745a722db2957d1d826268b5d713578ca23a9
ga060558 <ga060558@intelbras.com.br>
2024-08-02 14:06:46 -0300

    fix: inputs styles

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,64 +1,67 @@
-import { IconButton, InputAdornment } from "@material-ui/core";
-import TextField from '@mui/material/TextField';
+import {
+  IconButton,
+  InputAdornment,
+} from "@material-ui/core";
+import TextField from "@mui/material/TextField";
 import { Visibility, VisibilityOff } from "@material-ui/icons";
 import { SxProps, TextFieldVariants } from "@mui/material";
 import { useField } from "formik";
 import { useState } from "react";
 
-const classes = {
-  color: 'primary'
-}
-
 interface TextInputProps {
-  id?: string
-  sx?: SxProps
-  removeUnderline?: boolean
-  variant?: TextFieldVariants
+  id?: string;
+  sx?: SxProps;
+  removeUnderline?: boolean;
+  variant?: TextFieldVariants;
   label: string;
   name: string;
   type?: string;
   placeholder?: string;
   disabled?: boolean;
-  value?: string
+  value?: string;
+  suffix?: string;
 }
 
-export default function TextInput({ label, ...props }: TextInputProps) {
-  const [field, meta] = useField(props.name);
-  const [showPassword, setShowPassword] = useState(false)
+export default function TextInput(props: TextInputProps) {
+  const [field, meta] = useField(props);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleClickShowPassword = () => {
-    setShowPassword(!showPassword)
-  }
+    setShowPassword(!showPassword);
+  };
 
-  const fieldProps = {
-    endAdornment: props.type === 'password' ? (
-      <InputAdornment position="end">
+  const fieldProps={
+    endAdornment:
+      props.type === "password" ? (
+        <InputAdornment position="end">
           <IconButton
             aria-label="toggle password visibility"
             onClick={handleClickShowPassword}
             edge="end"
           >
-            {showPassword ? <VisibilityOff color="inherit" /> : <Visibility color="inherit" />}
+            {showPassword ? (
+              <VisibilityOff color="inherit" />
+            ) : (
+              <Visibility color="inherit" />
+            )}
           </IconButton>
         </InputAdornment>
-    ) : null
-  }
+      ) : null,
+  };
 
   return (
     <TextField
       id={props.id || props.name}
-      color='primary'
-      label={label}
-      variant={!props.variant ? 'outlined' : props.variant}
-      type={props.type === 'password' ? (showPassword ? 'text' : 'password') : props.type}
+      variant={!props.variant ? "outlined" : props.variant}
       fullWidth
       {...field}
       {...props}
-      sx={{...props.sx, mb: 2, color: "#fffff" }}
+      sx={{ ...props.sx }}
       value={props.value || field.value}
       error={Boolean(meta.touched && meta.error)}
-      helperText={meta.touched && meta.error ? meta.error : ''}
+      helperText={meta.touched && meta.error ? meta.error : ""}
+      type={props.type === 'password' ? (showPassword ? 'text' : 'password') : props.type}
       InputProps={fieldProps}
     />
   );
-};
+}
