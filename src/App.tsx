e2ffc0448ecb54aa645c8f96578b7aff018f34c3
ga060558 <ga060558@intelbras.com.br>
2024-08-02 14:06:26 -0300

    feat: add auth persistance  in order to keep the user logged whenever the page is reloaded

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,67 @@ import { MainPage } from "./components";
 import { Login, Tasks, Users } from "./pages";
 import { ThemeProvider } from "@mui/material";
 import ButtonAppBar from "./components/ButtonAppBar";
-import GroupIcon from '@mui/icons-material/Group';
-import HomeIcon from '@material-ui/icons/Home';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+import GroupIcon from "@mui/icons-material/Group";
+import HomeIcon from "@material-ui/icons/Home";
+import AssignmentIcon from "@mui/icons-material/Assignment";
 import { useSelector } from "react-redux";
-import { RootState } from "./store/store";
+import { Persistor, RootState } from "./store/store";
 import { selectIsAuthenticated } from "./store/modules/auth/slice";
 import theme from "./theme";
-import { useTheme } from "@emotion/react";
-
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
-    const isAuthenticated = useSelector((state: RootState) => selectIsAuthenticated(state));
+  const isAuthenticated = useSelector((state: RootState) =>
+    selectIsAuthenticated(state)
+  );
 
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                <BrowserRouter>
-                    {isAuthenticated && (
-                        <ButtonAppBar
-                            buttons={[
-                                { title: 'Home', icon: HomeIcon, route: '/home', message: 'Go to Home' },
-                                { title: 'Users', icon: GroupIcon, route: '/users', message: 'View Users' },
-                                { title: 'Tasks', icon: AssignmentIcon, route: '/tasks', message: 'Tasks' }
-                            ]}
-                        />
-                    )}
-                    <Routes>
-                        <Route path="/login" element={<Login />} />
-                        {isAuthenticated ? (
-                            <>
-                                <Route path="/home" element={<MainPage />} />
-                                <Route path="/users" element={<Users />} />
-                                <Route path="/tasks" element={<Tasks />} />
-                                <Route path="*" element={<Navigate to="/home" />} /> {/* Redireciona para /home se tentar acessar uma rota não encontrada */}
-                            </>
-                        ) : (
-                            <Route path="*" element={<Navigate to="/login" />} />
-                        )}</Routes>
-                </BrowserRouter>
-            </ThemeProvider>
-        </>
-    );
+  return (
+    <>
+      <PersistGate loading={null} persistor={Persistor}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            {isAuthenticated && (
+              <ButtonAppBar
+                buttons={[
+                  {
+                    title: "Home",
+                    icon: HomeIcon,
+                    route: "/home",
+                    message: "Go to Home",
+                  },
+                  {
+                    title: "Users",
+                    icon: GroupIcon,
+                    route: "/users",
+                    message: "View Users",
+                  },
+                  {
+                    title: "Tasks",
+                    icon: AssignmentIcon,
+                    route: "/tasks",
+                    message: "Tasks",
+                  },
+                ]}
+              />
+            )}
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              {isAuthenticated ? (
+                <>
+                  <Route path="/home" element={<MainPage />} />
+                  <Route path="/users" element={<Users />} />
+                  <Route path="/tasks" element={<Tasks />} />
+                  <Route path="*" element={<Navigate to="/home" />} />{" "}
+                </>
+              ) : (
+                <Route path="*" element={<Navigate to="/login" />} />
+              )}
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
+    </>
+  );
 }
 
 export default App;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,30 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from './modules/auth/slice'
 import { api } from "../services/api";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const roodReducer = combineReducers({
-    auth: authReducer
+const rootReducer = combineReducers({
+    auth: authReducer,
+    [api.reducerPath]: api.reducer,
 })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        [api.reducerPath]: api.reducer,
-    },
+    reducer: persistedReducer,
     middleware:(getDefaultMiddleware: any) => 
         getDefaultMiddleware().concat(api.middleware)
 })
 
+const Persistor = persistStore(store)
+
+export {Persistor}; 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store
